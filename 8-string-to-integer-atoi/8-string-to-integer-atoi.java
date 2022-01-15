94b2e61ca357bd3f15b6b6ddbf34af29cfacfece
Shubham Samdani <shubhamsamdanis@gmail.com>
2022-01-15 10:11:49 +0530

    Time: 2 ms (72.50%), Space: 38.9 MB (80.09%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.java
@@ -1,43 +1,73 @@
 class Solution {
-    public int myAtoi(String str) {
-        int i = 0;
-		int no =0;
-		boolean neg = false;
-
-		// triming white spaces
-		while(i<str.length() && str.charAt(i)==' '){
-			i++;
-		}
-
-		if(i<str.length() && str.charAt(i) == '-'){
-			neg = true;
-			i++;
-		} else if(i<str.length() && str.charAt(i) == '+'){
-			i++;
-		}
-
-		// now everything has to be no
-		while(i<str.length() && Character.isDigit(str.charAt(i))){
-			int c = str.charAt(i) -'0';
-			if((Integer.MAX_VALUE - c)/10 <no){
-				if(neg){
-					return Integer.MIN_VALUE;
-				} else{
-					return Integer.MAX_VALUE;
-				}
-			}
-			no = no*10 +  (c);
-			i++;
-		}
-
-		if(no==0){
-			return 0;
-		} else{
-			if(neg){
-				return -no;
-			} else
-				return no;
-		}
-
+    public int myAtoi(String s) {
+        char[] arr = s.toCharArray();
+        long res = 0;
+        int sign = 1;
+        int index = 0;
+        boolean signFound = false;
+        boolean integerFound = false;
+        while(index < arr.length && arr[index] == ' '){
+            index++ ;
+        }
+        
+        boolean foundFration = false;
+        for(int i = index; i < arr.length; i++){
+            //System.out.print( arr[i]+ ", ");
+            if(arr[i] == ' '){
+              break;  
+            }
+            else if(arr[i] == '-') {
+                if(integerFound){
+                    break;
+                }
+                
+                if(signFound){
+                    break;
+                } else{
+                    sign = -1;  
+                    signFound = true;
+                }
+               
+            } else if(arr[i] == '+'){
+                if(integerFound){
+                    break;
+                }
+                
+                if(signFound){
+                    break;
+                } else{
+                    sign = 1;
+                    signFound = true;
+                }
+            }
+            else if((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z')){
+                break;
+            } else if(arr[i] == '.'){
+                break;
+            } else {
+                res *= 10;
+                res += (arr[i] - '0');
+                integerFound = true;
+                if(sign == 1 && res > Integer.MAX_VALUE){
+                    return Integer.MAX_VALUE;
+                } else if(sign == -1){
+                    long temp = res;
+                    temp = temp * -1;
+                    if(temp < Integer.MIN_VALUE){
+                        return Integer.MIN_VALUE;
+                    }
+                }
+            }           
+        }
+        
+        res = res * sign;
+        int ans = (int)res;
+        if(sign == 1 && res > Integer.MAX_VALUE){
+            ans = Integer.MAX_VALUE;
+        } else if(sign == -1 && res < Integer.MIN_VALUE){
+            ans = Integer.MIN_VALUE;
+        }
+        
+        return ans; 
     }
 }
