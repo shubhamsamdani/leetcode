cad6d67b4fccdd3c2a4eacb15efd55cccf8d833f
Shubham Samdani <shubhamsamdanis@gmail.com>
2022-02-19 15:06:03 +0530

    Time: 2 ms, Memory: 50.4 MB - LeetHub

---
+++ single-number/single-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for(int i : nums){
+            num ^= i;
+        }
+        
+        return num;
+    }
+}
