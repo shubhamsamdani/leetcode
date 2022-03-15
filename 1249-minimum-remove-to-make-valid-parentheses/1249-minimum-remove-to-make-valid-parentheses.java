077cdb9cea8096634c923c006907592f4454323b
Shubham Samdani <shubhamsamdanis@gmail.com>
2022-03-15 22:08:51 +0530

    Time: 42 ms (35.91%), Space: 54.3 MB (26.80%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,43 @@
+class Solution {
+    
+    private class Pair {
+        int index;
+        char bracket;
+        
+        public Pair(int index, char bracket) {
+            this.index = index;
+            this.bracket = bracket;
+        }
+        
+    }
+    
+    public String minRemoveToMakeValid(String s) {
+        int count = 0;
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Pair> st = new Stack<>();
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '(') {
+                st.push(new Pair(i, sb.charAt(i)));
+            }
+            else if (sb.charAt(i) == ')') {
+                if (st.isEmpty()) 
+                    st.push(new Pair(i, sb.charAt(i)));
+                else {
+                    Pair top = st.peek();
+                    if (top.bracket == '(')
+                        st.pop();
+                    else {
+                        st.push(new Pair(i, sb.charAt(i)));
+                    }
+                }
+            }
+        }
+        
+        while (!st.isEmpty()) {
+            Pair top = st.pop();
+            sb = sb.deleteCharAt(top.index);
+        }
+        
+        return sb.toString();
+    }
+}
