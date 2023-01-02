2778859129ba800d2edfc86a53ae06f6e056d0b6
Shubham Samdani <shubhamsamdanis@gmail.com>
2023-01-02 23:28:09 +0530

    Time: 3 ms (28.25%), Space: 42.7 MB (27.63%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+         boolean firstCapital = false;
+        boolean allSmall = false;
+        boolean allCap = false;
+        
+        char[] arr = word.toCharArray();
+        if(word.length() == 1){
+            return true;
+        }
+        
+        if(!('A' <= arr[0] && arr[0] <= 'Z')){
+            allSmall = true;
+        }
+        
+        if('A' <= arr[1] && arr[1] <= 'Z'){
+            allCap = true;
+        } else{
+            firstCapital = true;
+        }
+        
+        for(int i = 1; i < arr.length; i++){
+            if(Character.isUpperCase(arr[i])){
+                if(firstCapital)
+                    return false;
+                
+                if(allSmall)
+                    return false;
+            } else if(Character.isLowerCase(arr[i])){
+                if(allCap){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
