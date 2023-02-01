6f0f40ff95ed98aaac3f15f15c62d7b0dd354605
Shubham Samdani <shubhamsamdanis@gmail.com>
2023-02-01 19:46:40 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (87.20%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if(str1.length() < str2.length()){
+            return gcdOfStrings(str2, str1);
+        } else if(!str1.startsWith(str2)){
+            return "";
+        } else if(str2.isEmpty()){
+            return str1;
+        }
+        
+        return gcdOfStrings(str2, mod(str1,str2));
+    }
+    
+    String mod(String str1, String str2){
+        while(str1.startsWith(str2)){
+            str1 = str1.substring(str2.length());
+        }
+        
+        return str1;
+    }
+}
